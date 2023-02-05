4318555924b71a5ad745fdda84be6257e6124e68
Moh7ven <Mohamedsangare0107@gmail.com>
2023-02-05 01:04:29 +0000

    login and registration interface

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,73 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { Pressable, StyleSheet, View, Text } from "react-native";
+import React, { useState } from "react";
+import { Input, Button } from "@rneui/base";
 
-const LoginScreen = () => {
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Connexion</Text>
+    <View style={styles.container}>
+      <Input
+        label="Email"
+        placeholder="Entrer votre email"
+        leftIcon={{ type: "material", name: "email" }}
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+
+      <Input
+        label="Mot de passe"
+        placeholder="Entrer votre mot de passe"
+        leftIcon={{ type: "material", name: "lock" }}
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+
+      <View>
+        <Button
+          title="Se connecter"
+          onPress={() => {
+            console.log("Bouton clic");
+          }}
+          style={styles.button}
+        />
+
+        <Pressable
+          onPress={() => navigation.navigate("Inscription")}
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed ? "rgb(210, 230, 255)" : "white",
+            },
+            styles.wrapperCustom,
+          ]}
+        >
+          {({ pressed }) => (
+            <Text style={styles.text}>
+              {pressed ? "Merci!" : "Nouveau? Inscrivez-vous"}
+            </Text>
+          )}
+        </Pressable>
+      </View>
     </View>
   );
 };
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+  button: {
+    width: 150,
+    margin: 20,
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 10,
+  },
+  containerButton: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  text: { padding: 10 },
+});
--- screens/RegistrationScreen.js
+++ screens/RegistrationScreen.js
@@ -1,12 +1,65 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { Pressable, StyleSheet, View, Text } from "react-native";
+import React, { useState } from "react";
+import { Input, Button } from "@rneui/base";
 
-const RegistrationScreen = () => {
+const RegistrationScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>Registration</Text>
+    <View style={styles.container}>
+      <Input
+        label="Nom"
+        leftIcon={{
+          type: "material",
+          name: "badge",
+        }}
+        placeholder="Entrez votre nom"
+      />
+      <Input
+        label="Email"
+        leftIcon={{
+          type: "material",
+          name: "email",
+        }}
+        placeholder="Entrez votre email"
+      />
+      <Input
+        label="Mot de passe"
+        leftIcon={{
+          type: "material",
+          name: "lock",
+        }}
+        placeholder="Entrez votre Mot de passe"
+      />
+      <Input
+        label="Photo"
+        leftIcon={{
+          type: "material",
+          name: "face",
+        }}
+        placeholder="Enregistrez  une photo"
+      />
+
+      <View>
+        <Button style={styles.button} title="S'enregistrez" />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    width: 150,
+    margin: 20,
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 10,
+  },
+  containerButton: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  text: { padding: 10 },
+});
 
 export default RegistrationScreen;
